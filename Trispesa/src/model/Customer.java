e41a2fca1d626a5ff58f6469f077c7002906e2de
laolr <laolr@160.97.138.29>
2019-12-26 18:04:29 +0100

    Added beans and DBManager.
    Added beans:
    Category;
    Customer;
    Product.

---
+++ Trispesa/src/model/Customer.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.util.Date;
+
+public class Customer {
+
+	private int ID;
+	private String username;
+	private String password;
+	private String name;
+	private String surname;
+	private String email;
+	private Date birthDate;
+	private Date registrationDate;
+	// arraylist di ordini (chiusi e/o in consegna)
+	// carrello
+	// arraylist di indirizzi di consegna
+	// arraylist di metodi di pagamento
+	
+}

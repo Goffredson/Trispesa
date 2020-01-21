f6740f13275d43eaff8a3b043e7b6d0d22de3426
laolr <laolr@LAPTOP-8VNS6LCU>
2020-01-21 10:30:57 +0100

    Aggiunti tantissimi jar.

--- Temp/src/controller/ManageProduct.java
+++ Temp/src/controller/ManageProduct.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+import drive.CreateGoogleFile;
 import exceptions.DBOperationException;
 import model.Category;
 import model.Product;
@@ -46,20 +46,26 @@ public class ManageProduct extends HttpServlet {
 					fileSaveDir.mkdirs();
 				}
 				String fileName = null;
-				String imagePath = null;
+				String imageId = null;
 				// Get all the parts from request and write it to the file on server
+				com.google.api.services.drive.model.File googleFile = null;
 				for (Part part : req.getParts()) {
 					if (part.getContentType() != null && Pattern.matches("image/.+", part.getContentType())) {
 						String ext = part.getContentType().substring(6);
 						fileName = req.getParameter("barcode") + "." + ext;
-						imagePath = uploadFilePath + File.separator + fileName;
-						part.write(imagePath);
+
+						googleFile = CreateGoogleFile.createGoogleFile(null, part.getContentType(), fileName,
+								part.getInputStream());
+						imageId = googleFile.getId();
+
+//						imagePath = uploadFilePath + File.separator + fileName;
+//						part.write(imagePath);
 					}
 				}
-				if (fileName == null) {
-					fileName = "imageNotFound.png";
-					imagePath = uploadFilePath + File.separator + fileName;
+				if (imageId == null) {
+					imageId = "1DbMKHR-mObaG56QAVDqGHoO4XoXStC2M";
 				}
+				
 
 				Product product;
 				long barcode = Long.parseLong(req.getParameter("barcode"));
@@ -75,11 +81,11 @@ public class ManageProduct extends HttpServlet {
 				long quantity = Integer.parseInt(req.getParameter("quantity"));
 				String offbrand = (String) req.getParameter("offbrand");
 				if (offbrand.equals("yes"))
-					product = new Product(0, barcode, name, brand, weight, superMarket, category, true, price,
-							quantity, discount, imagePath, false);
+					product = new Product(0, barcode, name, brand, weight, superMarket, category, true, price, quantity,
+							discount, imageId, false);
 				else
 					product = new Product(0, barcode, name, brand, weight, superMarket, category, false, price,
-							quantity, discount, imagePath, false);
+							quantity, discount, imageId, false);
 
 				DBManager.getInstance().addProduct(product);
 
@@ -103,21 +109,27 @@ public class ManageProduct extends HttpServlet {
 					fileSaveDir.mkdirs();
 				}
 				String fileName = null;
-				String imagePath = null;
+				String imageId = null;
 				// Get all the parts from request and write it to the file on server
+				com.google.api.services.drive.model.File googleFile = null;
 				for (Part part : req.getParts()) {
 					if (part.getContentType() != null && Pattern.matches("image/.+", part.getContentType())) {
 						String ext = part.getContentType().substring(6);
 						fileName = req.getParameter("barcode") + "." + ext;
-						imagePath = uploadFilePath + File.separator + fileName;
-						part.write(imagePath);
+
+						googleFile = CreateGoogleFile.createGoogleFile(null, part.getContentType(), fileName,
+								part.getInputStream());
+						imageId = googleFile.getId();
+
+//						imagePath = uploadFilePath + File.separator + fileName;
+//						part.write(imagePath);
 					}
 				}
 
 				long id = Long.parseLong(req.getParameter("old"));
 				Product product = DBManager.getInstance().getProductById(id);
-				if (fileName == null) {
-					fileName = product.getImagePath().substring(16);
+				if (imageId == null) {
+					imageId = product.getImagePath().substring(43);
 				}
 				long barcode = Long.parseLong(req.getParameter("barcode"));
 				String name = (String) req.getParameter("name");
@@ -133,10 +145,10 @@ public class ManageProduct extends HttpServlet {
 				String offbrand = (String) req.getParameter("offbrand");
 				if (offbrand.equals("yes"))
 					product = new Product(id, barcode, name, brand, weight, superMarket, category, true, price,
-							quantity, discount, imagePath, false);
+							quantity, discount, imageId, false);
 				else
 					product = new Product(id, barcode, name, brand, weight, superMarket, category, false, price,
-							quantity, discount, imagePath, false);
+							quantity, discount, imageId, false);
 
 				DBManager.getInstance().modifyProduct(product);
 

46483801f7838120eb4954c3348d9de8277eabc6
laolr <laolr@LAPTOP-8VNS6LCU>
2020-01-21 11:41:55 +0100

    Piccolo fix del commit precedente.

--- Temp/src/controller/Home.java
+++ Temp/src/controller/Home.java
@@ -8,15 +8,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import drive.GoogleDriveUtils;
 import persistence.DBManager;
 
 public class Home extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		GoogleDriveUtils.setContextPath(getServletContext().getRealPath("/"));
-		
 		//req.setAttribute("listaCategorieFoglia", DBManager.getInstance().getLeafCategories());
 		req.setAttribute("listaMacroCategorie", DBManager.getInstance().getMacroCategories());
 		RequestDispatcher rd = req.getRequestDispatcher("index.jsp");

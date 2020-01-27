5c271b3b6ce00ab69003665c3d8d9a26a118b7b5
Giorgio Andronico <bytecodex2@gmail.com>
2020-01-27 12:17:09 +0100

    Commit rapido su dieta

--- Temp/WebContent/js/diet.js
+++ Temp/WebContent/js/diet.js
@@ -7,17 +7,30 @@ function storeLeafCategory(id, name) {
 
 function addField() {
 	selectCount++;
-	$("#dietForm")
-			.append(
-					'<div class="form-group">'
-							+ '<label for="sel1">Categoria:</label> <select class="form-control" id="dietSelect_'
-							+ selectCount + '">');
+
+	formGroup = $('<div class="form-group">').appendTo($("#dietForm"));
+
+	formGroup.append(
+
+	'<label for="sel1">Categoria:</label>'
+			+ '<select class="form-control" name="dietSelect_' + selectCount
+			+ '" id="dietSelect_' + selectCount + '">');
 	for ( let key in leafCategories) {
 		$(String("#dietSelect_" + selectCount)).append(
 				'<option>' + leafCategories[key] + '</option>')
 	}
-	$("#dietForm").append(
-			'</select>' + '<div class="checkbox">'
-					+ '<label><input type="checkbox" value="">Di marca</label>'
-					+ '</div>' + '</div>');
+	formGroup
+			.append('</select>'
+					+ '<div class="checkbox">'
+					+ '<div class="form-check"> <input class="form-check-input" type="radio" name="offBrand" id="exampleRadios1" value="true" checked> <label class="form-check-label" for="exampleRadios1"> Non di marca </label>'
+					+ '</div>'
+					+ '<div class="form-check">'
+					+ '<input class="form-check-input" type="radio" name="offBrand" id="exampleRadios2" value="false">'
+					+ '<label class="form-check-label" for="exampleRadios2">'
+					+ 'Di marca'
+					+ '</label>'
+					+ '</div>'
+					+ '</div>'
+					+ '<div class="col-xs-2"> <label for="ex1">Grammi</label> <input class="col-xs-1" name="dietQuantity_'
+					+ selectCount + '" type="text">' + '</div>');
 }
